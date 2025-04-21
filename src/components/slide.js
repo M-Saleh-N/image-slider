fba6a8ba55d7d0cd112ab888dd0309ce12233063
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:07:24 +0300

    added last link

--- src/components/slide.js
+++ src/components/slide.js
@@ -7,7 +7,8 @@ const images = [
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-cape-elizabeth-maine-1024x683.jpg",
     "https://velvetescape.com/wp-content/uploads/2009/05/FE5350FA-175D-46E6-B872-E0003220853B-640x640.jpeg",
     "https://www.travelandleisure.com/thmb/9EVIxtEEdCN3BQ7G9PK2BD0P4Rg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-azores-portugal-WRLDSNST1122-5b47f58cb5ed428aa87ea8eb66c9f2ed.jpg",
-    
+    "https://berqwp-cdn.sfo3.cdn.digitaloceanspaces.com/cache/be-lavie.com/wp-content/uploads/2018/12/IMG_0857-1-1-1024x768-jpeg.webp",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRgGmKqyMF9VvQJnhy0U6B4LJhgWKsx-1MHw&s"
 
 ];
 
