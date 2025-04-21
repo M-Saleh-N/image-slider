3ad12fde858514c652365a8108773679d0d81bfb
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:11:40 +0300

    linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ImageSlider from './components/slide';
+import SimpleSlider from './components/slide';
 
 function App() {
   return (
     <div className="App">
- 
+      <SimpleSlider/>
     </div>
   );
 }
