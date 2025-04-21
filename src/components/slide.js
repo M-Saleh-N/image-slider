34f3910e47895cd805103801ded4612bdd420cf1
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:05:05 +0300

    added third image link

--- src/components/slide.js
+++ src/components/slide.js
@@ -5,6 +5,8 @@ import './slide.css'
 const images = [
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-eagle-beach-aruba-1024x683.jpg",
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-cape-elizabeth-maine-1024x683.jpg",
+    "https://velvetescape.com/wp-content/uploads/2009/05/FE5350FA-175D-46E6-B872-E0003220853B-640x640.jpeg",
+    
 
 ];
 
