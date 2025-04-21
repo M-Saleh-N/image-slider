328b16105b89f8c8b607dcae8f854e0fda1a1948
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:05:37 +0300

    added

--- src/components/slide.js
+++ src/components/slide.js
@@ -6,6 +6,7 @@ const images = [
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-eagle-beach-aruba-1024x683.jpg",
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-cape-elizabeth-maine-1024x683.jpg",
     "https://velvetescape.com/wp-content/uploads/2009/05/FE5350FA-175D-46E6-B872-E0003220853B-640x640.jpeg",
+    "https://www.travelandleisure.com/thmb/9EVIxtEEdCN3BQ7G9PK2BD0P4Rg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/the-azores-portugal-WRLDSNST1122-5b47f58cb5ed428aa87ea8eb66c9f2ed.jpg",
     
 
 ];
