c849f3e447ffd0fb0feaaaaaaf7e27d7a3f5d238
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:03:53 +0300

    added an image link

--- src/components/slide.js
+++ src/components/slide.js
@@ -3,7 +3,8 @@ import './slide.css'
 
 
 const images = [
-
+    "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-eagle-beach-aruba-1024x683.jpg",
+    
 ];
 
 
