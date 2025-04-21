1a88d8d1453bd6c3d4af0bc4d6f606ce91874392
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-21 16:04:32 +0300

    added a second image link

--- src/components/slide.js
+++ src/components/slide.js
@@ -4,7 +4,8 @@ import './slide.css'
 
 const images = [
     "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-eagle-beach-aruba-1024x683.jpg",
-    
+    "https://www.celebritycruises.com/blog/content/uploads/2022/01/best-sunset-in-the-world-cape-elizabeth-maine-1024x683.jpg",
+
 ];
 
 
